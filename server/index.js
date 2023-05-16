6402a8f932b5200f519dbae3a6d9bb8bbbae8b0c
Linh Tran <81435790+thaolinhttran@users.noreply.github.com>
2023-05-15 23:44:25 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,6 +14,12 @@ dotenv.config();
 //init express app
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://craite-space.preview-domain.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 app.use(express.json({limit: '50mb'}));
 
 app.use('/api/v1/post', postRoutes);
